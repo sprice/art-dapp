0d179d674efd4a31ef3dbe112859f8f1a5ee850c
Shawn Price <shawnp@gmail.com>
2017-10-05 23:48:31 -0700

    Add rinkeby deploy details

--- truffle.js
+++ truffle.js
@@ -5,8 +5,14 @@ module.exports = {
       host: "localhost",
       port: 8545,
       network_id: "*", // Match any network id
-      // from: "0xf5136F6a8C2c8C559FD1468d81a3f7DC9d2dC26E" // Shawn's Metamask Address
-      from: ""
+      from: "0xfbdc6e518a261996bccfd7a5788a3b6d13a7d19d"
+    },
+    rinkeby: {
+      host: "localhost", // Connect to local geth
+      port: 8545,
+      from: "0xf5136F6a8C2c8C559FD1468d81a3f7DC9d2dC26E", // Shawn's Metamask Address
+      network_id: 4,  // Rinkeby network
+      gas: 4612388 // Gas limit used for deploys
     }
   }
 };
