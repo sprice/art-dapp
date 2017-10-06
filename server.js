d5d1fc0b2d90df592644622dd71ca173bdeed418
Shawn Price <shawnp@gmail.com>
2017-10-06 02:08:19 -0700

    tweaks to server

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express')
+const path = require('path')
 const server = express()
 
-server.use('/', express.static(__dirname + '/build_webpack'))
+server.use('/', express.static(path.join(__dirname, '/build_webpack')))
 
 server.get('/*', (req, res) => {
-  res.sendFile(__dirname + '/build_webpack/index.html')
+  res.sendFile(path.join(__dirname,'/build_webpack/index.html'))
 })
 
 const port = process.env.PORT
