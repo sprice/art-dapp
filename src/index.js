9fe3d39357f61d8b5cb3f104a162f705f2785811
Shawn Price <shawnp@gmail.com>
2017-10-09 21:59:51 -0700

    Add Google Analytics

--- src/index.js
+++ src/index.js
@@ -1,8 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Routes from './routes'
+const ReactGA = require('react-ga')
 
-ReactDOM.render(
-  <Routes />,
-  document.getElementById('root')
-);
+let isDev = true
+switch (window.location.hostname) {
+case "localhost":
+case "127.0.0.1":
+    break;
+default:
+    isDev = false
+}
+
+if (!isDev) ReactGA.initialize('UA-107878547-1')
+
+const render = () => {
+    if (!isDev) ReactGA.pageview(window.location.pathname)
+    ReactDOM.render(
+        <Routes />,
+        document.getElementById('root')
+    )
+}
+
+render()
