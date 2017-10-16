59744c30c4943ef582814869737f705478b69e3d
Shawn Price <shawnp@gmail.com>
2017-10-15 20:47:15 -0700

    Add contract link

--- src/About.js
+++ src/About.js
@@ -1,5 +1,7 @@
 import React from 'react'
 
+const contractHref = 'https://rinkeby.etherscan.io/address/0xd275a265038c9ab985bc96a81aaabcceef8f57b9#code'
+
 const About = ({ match }) => (
   <div className="app">
     <div className="primary">
@@ -17,6 +19,10 @@ const About = ({ match }) => (
           <div className="info">
             <h2>A new way to think about digital ownership</h2>
             <p>The Blockchain allows for trustless proof that an artwork created on the Chill platform is unique, rare, and authentic. There is also the added benefit that all functions of an artwork, from buying and selling to loaning, can occur with or without the Chill platform. Once an artwork is created, it is permanent. If Chill should ever shut down or dissapear, your art will continue to exist and remain yours.</p>
+          </div>
+          <div className="info">
+            <h2>Full Transparency</h2>
+            <p>If you&rsquo;re nerdy like us, you can view the <a href={contractHref}>source code</a> for our Ethereum smart contract.</p>
           </div>
         </div>
       </div>
